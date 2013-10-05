3b6be78e01d7e875771596d4ed217a9bd52bd43d
Hack Reactor Students <github@hackreactor.com>
2013-10-04 22:53:55 -0700

    completed basic requirement 3: added draggable and colored dot to represent player

--- collider.js
+++ collider.js
@@ -26,38 +26,32 @@
     gameStats.bestScore = _.max([gameStats.score, gameStats.bestScore]);
     d3.select('#best-score').text(gameStats.bestScore.toString());
   };
-  createEnemies = function() {
+  var createEnemies = function() {
     return _.range(0, gameOptions.nEnemies).map(function(i) {
       return {
         id: i,
-        x: parseFloat(Math.random() * 100,10),
-        y: parseFloat(Math.random() * 100,10)
+        x: Math.random() * 100,
+        y: Math.random() * 100
       };
     });
   };
-  // var createEnemies = function(){
-  //    var value =  _.map(_.range(0, gameOptions.nEnemies), function(value, key, list){
-  //      return {id:value, x: Math.random()*100, y: Math.random()*100};
-  //    // return _.map(_.range(0, gameOptions.nEnemies), function(value, key, list){
-  //    //   return {id:value, x: Math.random()*100, y: Math.random()*100};
-  //   // return _.range(0, gameOptions.nEnemies).map(function(i) {
-  //   //   return {
-  //   //     id: i,
-  //   //     x: Math.random() * 100,
-  //   //     y: Math.random() * 100
-  //   //   };
-  //   });
-  //   console.log (value);
-  //   return value;
-  // };
+
+  // var player = gameBoard.select('circle.player').data([gameOptions.width/2, gameOptions.height/2]);
+  var player = gameBoard.append('svg:circle').attr('cx', 200).attr('cy', 200).attr('r', 10).attr('fill', 'red').call(d3.behavior.drag().on('drag', move));
+  
+  function move() {
+    var dragTarget = d3.select(this);
+    d3.select(this)
+      .attr('cx', function() { return d3.event.dx + parseInt(d3.select(this).attr('cx'))})
+      .attr('cy', function() { return d3.event.dy + parseInt(d3.select(this).attr('cy'))})
+  };
+
   var render = function(enemy_data){
-    var enemies = gameBoard.selectAll('circle.enemy').data(enemy_data, function(d){return d.id;});
-    // var enemies = gameBoard.selectAll('circle.enemy').data(enemy_data, function(d){return d.id;});
+    var enemies = gameBoard.selectAll('circle.enemy').data(enemy_data, function(d){ return d.id; });
 
     enemies.enter().append('svg:circle')
       .attr('class','enemy')
       .attr('cx', function(enemy){
-        
         return axes.x(enemy.x);
       })
       .attr('cy', function(enemy){
@@ -65,14 +59,10 @@
       })
       .attr('r', 10);
 
-    // console.log('after enter ',enemies);
-
     enemies.exit().remove();
 
     var tweenMove = function(endData){
-      // console.log(endData)
       var enemy = d3.select(this);
-      console.log(enemy)
       var startPos = {
         x: parseFloat(enemy.attr('cx')),
         y: parseFloat(enemy.attr('cy'))
@@ -82,44 +72,27 @@
         x: axes.x(endData.x),
         y: axes.y(endData.y)
       };
-      console.log(endPos)
 
       return function(t) {
-        // check collision
         var enemyNextPos = {
-          x: parseFloat(startPos.x + (endPos.x - startPos.x) * t, 10),
-          y: parseFloat(startPos.y + (endPos.y - startPos.y) * t, 10)
+          x: startPos.x + (endPos.x - startPos.x) * t,
+          y: startPos.y + (endPos.y - startPos.y) * t
         };
-        // console.log(enemyNextPos);
-    
         return enemy.attr('cx', enemyNextPos.x).attr('cy', enemyNextPos.y);
       };
     };
-    // debugger;
     return gameBoard.selectAll('circle.enemy').transition().duration(1000).tween('custom', tweenMove);
-    // console.log(enemies);
-    // return enemies.transition().duration(1000).tween('custom', tweenMove);
-    // return gameBoard.selectAll('circle.enemy');
   };
-
-  
-  
-
 
   var play = function(){
     var gameTurn = function(){
       var newEnemyPositions = createEnemies();
-      // console.log(newEnemyPositions);
       render(newEnemyPositions);
-    }
-    var newEnemyPositions = createEnemies();
-    gameTurn(newEnemyPositions);
-    setInterval(gameTurn,1000);
+    };
 
+    gameTurn();
+    setInterval(gameTurn,1000);
   };
-  // debugger;
   play();
 
-
 }).call(this);
-
